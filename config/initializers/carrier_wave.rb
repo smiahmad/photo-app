692cefc8d3948211c6c4f3da0b505f7530ca4ac2
Imran Ahmad <imran.ahmad@navis.com>
2022-10-15 15:28:48 -0700

    added image upload feature

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog.credentials = {
-      :provider => 'AWS'
+      :provider => 'AWS',
       :aws_access_key_id => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
